d12e8a90f1d839873fd5f14b5055104546e759fe
alex-stiegel <alex.kasa@mymedwellness.com>
2021-10-20 22:41:34 -0400

    change categories to props, move data to app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Category from './Category/Category.js';
 
 
 function App() {
+    const tasks = [
+        { "name": "Task Name 1", "description": "this is a description", "rating": 1, "off": false },
+        { "name": "Task Name 2", "description": "this is a description", "rating": 2, "off": false },
+        { "name": "Task Name 3", "description": "this is a description", "rating": 3, "off": false },
+        { "name": "Task Name 4", "description": "this is a description", "rating": 4, "off": false },
+        { "name": "Task Name 5", "description": "this is a description", "rating": 5, "off": false },
+        { "name": "Task Name 6", "description": "this is a description", "rating": 6, "off": false },
+        { "name": "Task Name 7", "description": "this is a description", "rating": 7, "off": false },
+        { "name": "Task Name 8", "description": "this is a description", "rating": 8, "off": false },
+        { "name": "Task Name 9", "description": "this is a description", "rating": 4, "off": true },];
+
     return (
         <div className="App">
             <header className="App-header">
                 Task Tracker
             </header>
             <div className='App-body'>
-                <Category />
+                <Category name="Category Name" tasks={tasks} />
             </div>
         </div>
     );
--- src/Category/Category.js
+++ src/Category/Category.js
@@ -29,23 +29,17 @@ const taskRatingToColor = (rating) => {
 function Category(props) {
     return (
         <div className='Category-container'>
-            <h2 className="Category-Name">Category Name</h2>
+            <h2 className="Category-Name">{props.name}</h2>
             <Card className="Category-Card" variant='outlined'>
                 <div className='Task-new-container'>
                     <form>
-                        <textarea className='Task-new' rows='1' name="Task-new" placeholder='Category Name' />
+                        <textarea className='Task-new' rows='1' name="Task-new" placeholder={props.name} />
                     </form>
                 </div>
                 <div>
-                    <Task name="Task Name 1" description="Task Description" color={taskRatingToColor(1)} off={false} />
-                    <Task name="Task Name 2" description="Tsk Description" color={taskRatingToColor(2)} off={false} />
-                    <Task name="Task Name 3" description="Tak Description" color={taskRatingToColor(3)} off={false} />
-                    <Task name="Task Name 4" description="Tas Description" color={taskRatingToColor(4)} off={false} />
-                    <Task name="Task Name 5" description="TaskDescription" color={taskRatingToColor(5)} off={false} />
-                    <Task name="Task Name 6" description="Task escription" color={taskRatingToColor(6)} off={false} />
-                    <Task name="Task Name 7" description="Task Dscription" color={taskRatingToColor(7)} off={false} />
-                    <Task name="Task Name 8" description="Task Decription" color={taskRatingToColor(8)} off={false} />
-                    <Task name="Task Name Off" description="Task Decription" color={taskRatingToColor(3)} off={true} />
+                    {props.tasks.map((task) => (
+                        <Task name={task.name} description={task.description} color={taskRatingToColor(task.rating)} off={task.off} />
+                    ))}
                 </div>
             </Card>
         </div>
