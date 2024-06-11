e7bce49b285eb7775a01e56fd5c4a9fd87f0d8ec
vailveix <vailveix@gmail.com>
2024-06-10 21:06:13 -0400

    Update completeTask.php

--- API/completeTask.php
+++ API/completeTask.php
@@ -2,12 +2,13 @@
 header('Access-Control-Allow-Origin: *');
 require_once("includes/db_connect.php");
 
-$task_id = $_POST['task_id'];
+$data = json_decode(file_get_contents('php://input'), true);
+
+$task_id = $data['task_id'];
 $completion_date = '2024-06-04';
 
 if(!isset($task_id)){
-    //header('Status: 400');
-    print_r($_POST);
+    header('Status: 400');
     exit();
 }
 
