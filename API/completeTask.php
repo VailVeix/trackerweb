88705faf088b9307967a734814a9e2e9452bb33d
vailveix <vailveix@gmail.com>
2024-06-10 20:44:36 -0400

    update complete task to take post

--- API/completeTask.php
+++ API/completeTask.php
@@ -1,9 +1,17 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 require_once("includes/db_connect.php");
 
 $task_id = $_POST['task_id'];
 $completion_date = '2024-06-04';
 
+if(!isset($task_id)){
+    //header('Status: 400');
+    print_r($_POST);
+    exit();
+}
+
 $stmt = $db->prepare('INSERT INTO task_completions (task_id, completion_date) values (?, ?)');
 $stmt->execute([$task_id, $completion_date]);
 header('Status: 200');
+exit();
